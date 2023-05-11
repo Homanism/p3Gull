b8874bc882b8b574082d66452eeb3559d4a766a1
Homanism <89544626+Homanism@users.noreply.github.com>
2023-05-11 23:39:09 +0200

    fix: working on header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="p3gull-2022-lottie-header">
-			<lottie-player class="lottie-header" aria-hidden="true" src="https://p3.no/wp-content/themes/p3v2/images/p3gull2022/univers/P3Gull_2022_Wireframe_KunEn.json" background="transparent" speed="1" style="width: 100%; height: auto; z-index:3;" loop="" autoplay=""></lottie-player>
+  <div>
+    <NavBar />
+
+		
 	</div>
 
 </template>
 
-<script setup>
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
   //
 </script>
