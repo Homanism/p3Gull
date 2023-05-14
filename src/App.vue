fbb702ca7f26570e6f19f2abc6bfb784341360e9
Homanism <89544626+Homanism@users.noreply.github.com>
2023-05-14 09:17:57 +0200

    fix: add AOS.init

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,26 @@
 </template>
 
 <script setup>
-  //
+
+import 'aos/dist/aos.css';
+
+import AOS from 'aos';
+
+
+
+
+AOS.init();
+
 </script>
+
+
+
+
+<style scoped>
+.v-application{
+display: block !important;
+margin-top: -24px;
+}
+/* CSS styles for your component */
+
+</style>
